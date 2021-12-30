c9e1fa11fcce2843695b9e7fb0f63c939af4ae3b
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2021-12-29 18:48:58 -0700

    Update ownership program with some learnings on slices

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -6,7 +6,9 @@ fn main() {
     //     3. When the owner goes out of scope, the value will be dropped.
     // For example, string literals (e.g., "hello") are immutable. However, a
     // String can be modified. If we want to create a String from a literal, we
-    // can use String::from("testX")
+    // can use String::from("testX"). A literal is in fact a slice of the
+    // binary, so its type is &str rather than String (will discuss in detail
+    // later)
     println!("Scope 1");
     {
         let mut s = String::from("test1"); // requests memory from the
@@ -86,6 +88,29 @@ fn main() {
         let r3 = &mut s;
         println!("\tr3 = {}", r3);
     }
+    // A slice is a data type which references a contiguous sequence of
+    // elements in a collection. It does not have ownership of the elements
+    println!("Scope 6");
+    {
+        let s = String::from("hello world");
+        let idx = get_end_idx_of_first_word(&s);
+        println!("\tIndex of first word is {}", idx);
+
+        // A slice allows us to refer to a portion of the string. This is safer
+        // than returning an index because the compiler will ensure that the
+        // references remain valid
+        let hello = &s[..5]; // can omit the start index if it's zero
+        let _world = &s[6..]; // can omit the end index if it's the length
+        let _all = &s[..]; // can omit both indices if we want the whole thing
+        let first_word = get_slice_of_first_word(&s);
+        println!("\tfirst_word = {}", first_word);
+        println!("\tfirst_word == hello is {}", first_word == hello);
+
+        // We can also slice arrays
+        let a = [1, -2, 3, -4, 5];
+        let a_slice = &a[1..3]; // slice type is &[i32]
+        assert_eq!(a_slice, &[-2, 3]);
+    }
 }
 
 fn takes_ownership(ss: String) {
@@ -109,4 +134,29 @@ fn uses_reference(ss: &String) {
 fn change_by_reference(ss: &mut String) {
     ss.push_str("content"); // causes an error
     println!("\tchange_by_reference: {}", ss);
+}
+
+fn get_end_idx_of_first_word(ss: &String) -> usize {
+    let bytes = ss.as_bytes(); // convert string to array of bytes
+                               // .iter() returns each element in the collection, and .enumerate() wraps
+                               // the element into a tuple along with its index
+    for (idx, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return idx;
+        }
+    }
+    ss.len()
+}
+
+fn get_slice_of_first_word(ss: &str) -> &str {
+    // &str is the type for a String slice
+    let bytes = ss.as_bytes(); // convert string to array of bytes
+                               // .iter() returns each element in the collection, and .enumerate() wraps
+                               // the element into a tuple along with its index
+    for (idx, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &ss[..idx];
+        }
+    }
+    &ss[..]
 }
