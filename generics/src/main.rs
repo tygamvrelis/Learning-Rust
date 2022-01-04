5228928aabb554a83b03c351c0d20c18f350fd7c
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2022-01-03 19:52:33 -0700

    Continue learning about traits

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -16,7 +16,17 @@
 // algorithm needs to perform elementwise comparisons, which means we must be
 // able to order the elements. Not all types T will be comparable in such a
 // way, so to restrict this function to only types which can be ordered, we use
-// a trait.
+// a trait. A different way to specify that a function takes any type that
+// implements traits X and Y would be to write that argument in the form of,
+// e.g., "list: &(impl X + Y)". What this really means is shown in its full
+// form below.
+// An alternate syntax option involves using the "where" clause, e.g.,
+// fn some_func<T, U>(t: &T, u: &U) -> i32
+//     where T: Display + Clone,
+//           U: Clone + Debug
+// { ... }
+// In summary, traits let us specify to the compiler circumstances under which
+// a generic type must have a certain set of behaviours.
 fn find_max<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut max = list[0];
     for &item in list {
@@ -42,6 +52,22 @@ impl<T> Point<T> {
     }
 }
 
+// Conditionally implements the cmp_display method, with this depending on the
+// traits that the type T implements. Using a similar technique, we can write
+// impl blocks for all generic types T which implement a set of traits (called
+// trait bounds); this is called a blanket implementation. An example is the
+// to_string() method, which is implemented for all types T which have the
+// Display trait
+impl<T: std::fmt::Display + PartialOrd> Point<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("x >+ y");
+        } else {
+            println!("x < y");
+        }
+    }
+}
+
 // Function available *only* for f32 type
 impl Point<f32> {
     fn dist_from_origin(&self) -> f32 {
@@ -58,7 +84,14 @@ impl Point<f32> {
 // another crate to be able to implement our trait for its own types, then we
 // need to declare it as pub.
 pub trait Summary {
-    fn summarize(&self) -> String;
+    // We can force the trait-implementer to provide their own implementation
+    // by simply declaring the method, i.e.:
+    fn summarize_author(&self) -> String;
+
+    // This is a default implementation of the trait method
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
 }
 
 pub struct Article {
@@ -68,6 +101,10 @@ pub struct Article {
 }
 
 impl Summary for Article {
+    fn summarize_author(&self) -> String {
+        format!("{}", self.author)
+    }
+
     fn summarize(&self) -> String {
         format!("{}, by {}", self.headline, self.author)
     }
@@ -79,8 +116,20 @@ pub struct Tweet {
 }
 
 impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+// Calling code doesn't know the concrete type that will be returned; has to
+// rely on the interface, so to speak. However, using this "impl trait" syntax
+// for the return type has the restriction that only one concrete type can be
+// returned (i.e., can't sometimes return an Article and other times return a
+// Tweet).
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("hunter2"),
+        content: String::from("Hello, world!"),
     }
 }
 
@@ -100,9 +149,13 @@ fn main() {
         float_struct.dist_from_origin()
     );
 
-    let tweet = Tweet {
-        username: String::from("hunter2"),
-        content: String::from("Hello, world!"),
-    };
+    let tweet = returns_summarizable();
     println!("Tweet: {}", tweet.summarize());
+
+    let article = Article {
+        author: String::from("hunter2"),
+        headline: String::from("Hello, world!"),
+        content: String::from("LOREM IPSUM!!"),
+    };
+    println!("Article: {}", article.summarize());
 }
