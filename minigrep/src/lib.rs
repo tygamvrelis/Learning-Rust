225d442fa9ab467df00b0dfda81305b6a092b706
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2022-01-12 23:26:22 -0500

    Refactor argument parsing and error handling. Read file

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,3 +1,6 @@
+use std::error::Error;
+use std::fs;
+
 // Configuration variables should be grouped into a single structure so that
 // their purpose becomes more clear.
 pub struct Config {
@@ -6,10 +9,25 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Config {
-        Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        let num_args = args.len();
+        if num_args < 3 {
+            return Err("not enough arguments");
+        }
+        Ok(Config {
             query: args[1].clone(),
             fname: args[2].clone(),
-        }
+        })
     }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    println!("Searching for {} in {}", config.query, config.fname);
+
+    let contents = fs::read_to_string(config.fname)?;
+    println!("Contents:\n{}", contents);
+
+    // Returning () is the idiomatic way to indicate that we are calling a
+    // function for its side effects only (doesn't return a value we need)
+    Ok(())
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,25 +1,27 @@
 use std::env;
-use std::fs;
+use std::process;
 
 use minigrep::Config;
-
-// TODO: Errors should be descriptive so that the user can identify the problems more
-// easily.
-// Would be nice if all error-handling logic was in one place so that future
-// maintainers of the code only had to consult one place if it needed to change
+use minigrep::run;
 
 // Main function should delegate its tasks to functions so that it becomes more
 // clear what the responsibilities of the program are and how they are
 // separated.
+// Would be nice if all error-handling logic was in one place so that future
+// maintainers of the code only had to consult one place if it needed to change.
+// Errors should be descriptive so that the user can identify the problems more
+// easily.
 fn main() {
     // Collect turns the args env::args() iterator into a collection. It can
     // create a variety of collections, so we need to explicitly annotate the
     // type we desire (in this case, a Vec<String>)
     let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args);
-    println!("Searching for {} in {}", config.query, config.fname);
-
-    let contents = fs::read_to_string(config.fname).expect("Error while reading file");
-    println!("Contents:\n{}", contents);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Argument parsing problem: {}", err);
+        process::exit(1);
+    });
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
