7aefae8c4988945b6ee15bf1b978d0cdcc15ada0
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2022-01-15 15:52:41 -0500

    Learn about iterators

--- functional_lang/src/main.rs
+++ functional_lang/src/main.rs
@@ -74,6 +74,29 @@ fn generate_workout(intensity: u32, random_number: u32) {
     }
 }
 
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32; // Needed for iterator trait, "associated type"
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
 fn main() {
     generate_workout(24, 7);
 }
@@ -107,4 +130,43 @@ fn call_with_str() {
     let v1 = c.value(&string1[..]);
 
     assert_eq!(v1, 12);
+}
+
+#[test]
+fn iterator_demo() {
+    let v1 = vec![4, 5, 6];
+
+    // calling next() changes internal state, therefore need mutable
+    let mut v1_iter = v1.iter(); // iterator over immutable references
+                                 // can call into_iter() to get val ownership
+                                 // can call iter_mut() to get mutable refs
+
+    assert_eq!(v1_iter.next(), Some(&4));
+    assert_eq!(v1_iter.next(), Some(&5));
+    assert_eq!(v1_iter.next(), Some(&6));
+    assert_eq!(v1_iter.next(), None);
+}
+
+#[test]
+fn iterator_sum() {
+    let v1 = vec![1, 2, 3];
+    let total: i32 = v1.iter().sum(); // sum is a "consuming adaptor"
+    assert_eq!(total, 6);
+}
+
+#[test]
+fn iterator_adaptor() {
+    let v1 = vec![1, 2, 3];
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+    assert_eq!(v2, vec![2, 3, 4]);
+}
+
+#[test]
+fn iterator_trait_methods_on_counter() {
+    let sum: u32 = Counter::new()
+        .zip(Counter::new().skip(1))
+        .map(|(a, b)| 2 * b - a)
+        .filter(|x| x % 2 == 0)
+        .sum();
+    assert_eq!(sum, 10);
 }
